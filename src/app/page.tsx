0d7c89099260ee872bdef325e28150ad95a44742
David <davidhurmanchuk@gmail.com>
2025-09-13 20:32:03 +0300

    fix footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,3 @@
-// app/layout.tsx
-import Footer from '@/components/layout/Footer';
-import Header from '@/components/layout/Header';
-import './globals.css';
-
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html lang="en">
-      <body>
-        <Header />
-        <main>{children}</main>
-        <Footer />
-      </body>
-    </html>
-  );
+export default function Home() {
+  return <div>Production ready</div>;
 }
