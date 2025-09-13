abd730793855787df9f692e778c6179556beecd2
DavidH <davidhurmanchuk@gmail.com>
2025-09-13 17:29:09 +0300

    add header (#8)
    * add header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,18 @@
-export default function Home() {
-  return <div>dlsksklsdkldlsk</div>;
+// app/layout.tsx
+import Header from '@/components/layout/Header';
+import './globals.css';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <Header />
+        <main>{children}</main>
+      </body>
+    </html>
+  );
 }
