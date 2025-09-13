892b36a2ba63aee6e946e15a1dba8cd28164fe63
David <davidhurmanchuk@gmail.com>
2025-09-13 17:25:00 +0300

    header fixN2

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,6 +1,5 @@
 import { Slot } from '@radix-ui/react-slot';
 import { cva, type VariantProps } from 'class-variance-authority';
-import { CheckIcon, ChevronDownIcon, ChevronUpIcon } from 'lucide-react';
 import * as React from 'react';
 
 import { cn } from '@/lib/utils';
