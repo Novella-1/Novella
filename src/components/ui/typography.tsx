bad75efc9339346fabdb67fe188ef4b3eee47232
alina <kostenkoalina97@icloud.com>
2025-09-13 18:22:39 +0200

    feat(TP1-12): add typography

---
+++ src/components/ui/typography.tsx
@@ -0,0 +1,69 @@
+export function TypographyH1() {
+  return (
+    <h1 className="font-manrope text-[32px] not-italic font-bold leading-[41px] tracking-[-0.32px] sm:text-[48px] sm:leading-[56px] sm:tracking-[-0.96px]">
+      Taxing Laughter: The Joke Tax Chronicles
+    </h1>
+  );
+}
+
+export function TypographyH2() {
+  return (
+    <h2 className="font-manrope text-[22px] not-italic font-bold leading-[30.8px] sm:text-[32px] sm:leading-[41px] sm:tracking-[-0.32px]">
+      The People of the Kingdom
+    </h2>
+  );
+}
+
+export function TypographyH3() {
+  return (
+    <h3 className="font-manrope text-[22px] not-italic font-semibold leading-[30.8px]">
+      The People of the Kingdom
+    </h3>
+  );
+}
+
+export function TypographyH4() {
+  return (
+    <h4 className="font-manrope text-[20px] not-italic font-semibold leading-normal">
+      The Joke Tax
+    </h4>
+  );
+}
+
+export function TypographyH5() {
+  return (
+    <h5 className="font-manrope text-base not-italic font-bold leading-6">
+      The Joke Tax
+    </h5>
+  );
+}
+
+//paragraph
+export function TypographyP() {
+  return (
+    <p className="font-manrope text-sm not-italic font-medium leading-[21px]">
+      The king, seeing how much happier his subjects were, realized the error of
+      his ways and repealed the joke tax.
+    </p>
+  );
+}
+
+//for buttons
+export function TypographyB() {
+  return (
+    <p className="font-manrope text-sm not-italic font-bold leading-[21px]">
+      The king, seeing how much happier his subjects were, realized the error of
+      his ways and repealed the joke tax.
+    </p>
+  );
+}
+
+//uppercase for header/footer
+export function TypographyU() {
+  return (
+    <p className="font-manrope text-xs not-italic font-bold leading-[11px] tracking-[0.12px] uppercase">
+      The king, seeing how much happier his subjects were, realized the error of
+      his ways and repealed the joke tax.
+    </p>
+  );
+}
