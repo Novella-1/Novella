f3f3f23613bbd1cf6479e0fd9112d0a58aa48b06
alina <kostenkoalina97@icloud.com>
2025-09-14 14:14:01 +0200

    feat(TP1-12): add icons

---
+++ src/components/ui/icons.tsx
@@ -0,0 +1,72 @@
+import {
+  Truck,
+  Search,
+  Heart,
+  ShoppingBag,
+  Headphones,
+  Menu,
+  House,
+  Plus,
+  Minus,
+  X,
+  ChevronLeft,
+  ChevronRight,
+  ChevronUp,
+  ChevronDown,
+} from 'lucide-react';
+
+export function VanIcon() {
+  return <Truck />;
+}
+
+export function SearchIcon() {
+  return <Search />;
+}
+
+export function HeartIcon() {
+  return <Heart />;
+}
+
+export function CartIcon() {
+  return <ShoppingBag />;
+}
+
+export function HeadphonesIcon() {
+  return <Headphones />;
+}
+
+export function BurgerMenuIcon() {
+  return <Menu />;
+}
+
+export function HomeIcon() {
+  return <House />;
+}
+
+export function PlusIcon() {
+  return <Plus />;
+}
+
+export function MinusIcon() {
+  return <Minus />;
+}
+
+export function CLoseIcon() {
+  return <X />;
+}
+
+export function ArrowUpIcon() {
+  return <ChevronUp />;
+}
+
+export function ArrowDownIcon() {
+  return <ChevronDown />;
+}
+
+export function ArrowLeftIcon() {
+  return <ChevronLeft />;
+}
+
+export function ArrowRightIcon() {
+  return <ChevronRight />;
+}
