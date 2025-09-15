92fa3d48e800553265a9cc6b3aaa1446edd6c322
Yurii <yurakupchunsky@gmail.com>
2025-09-16 00:32:19 +0300

    feat(TP1-39) : add cards catagory section

---
+++ src/components/common/BooksScrollArea/ScrollArea.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import React, { useRef } from 'react';
+import { CardItem } from '@/components/common/CardItem';
+import { HorizontalScroll, ScrollButtons } from '@/components/ui/scroll-area';
+
+export function CardCarousel({ books }: { books: object[] }) {
+  const scrollRef = useRef<HTMLDivElement>(null);
+
+  return (
+    <div className="relative w-[310px] md:w-[584px] xl:w-[1136px]">
+      <ScrollButtons scrollRef={scrollRef} />
+      <HorizontalScroll
+        ref={scrollRef}
+        items={books}
+      >
+        <CardItem />
+      </HorizontalScroll>
+    </div>
+  );
+}
---
+++ src/components/layout/CardsCarouselSection/CardsCarouselSection.tsx
@@ -0,0 +1,17 @@
+import { CardCarousel } from '@/components/common/BooksScrollArea/ScrollArea';
+import { SectionTitle } from '../../ui/typography';
+
+export function CardsCarouselSection({
+  title = 'You might like',
+  books = Array.from({ length: 7 }, () => ({})),
+}: {
+  title?: string;
+  books?: object[];
+}) {
+  return (
+    <>
+      <SectionTitle className="mb-[-40px]">{title}</SectionTitle>
+      <CardCarousel books={books} />
+    </>
+  );
+}
