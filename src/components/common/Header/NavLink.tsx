2ceef4a1e321e522b097772e842fa5fd2e8b4217
David <davidhurmanchuk@gmail.com>
2025-09-16 14:31:21 +0300

    feat: add improved search(mockups used)

--- src/components/common/Header/NavLink.tsx
+++ src/components/common/Header/NavLink.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+// eslint-disable-next-line import/no-named-as-default
 import clsx from 'clsx';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
@@ -19,20 +20,22 @@ const NavLink: FC<NavLinkProps> = ({ href, name, className }) => {
     <Link
       href={href}
       className={clsx(
-        'relative flex items-center justify-center font-bold transition-colors duration-200',
+        'relative font-bold transition-colors duration-200',
         isActive ? 'text-black' : 'text-[#BAA48C] hover:text-black',
         className,
       )}
     >
-      {name}
-      {isActive && (
-        <span
-          className={clsx(
-            'absolute left-0 w-full h-[2px] bg-black transition-all duration-300',
-            'top-[34px] xl:top-[42px]', // моб/таб = 34px, десктоп = 42px
-          )}
-        />
-      )}
+      <span className="relative inline-block">
+        {name}
+        {isActive && (
+          <span
+            className={clsx(
+              'absolute left-0 w-full h-[2px] bg-black transition-all duration-300',
+              'top-[20px] md:top-[34px] xl:top-[42px]',
+            )}
+          />
+        )}
+      </span>
     </Link>
   );
 };
