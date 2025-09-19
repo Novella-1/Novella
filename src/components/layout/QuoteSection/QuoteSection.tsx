f92c90d0b573671b2ce88616ccc6a7c02ba644f2
Aonarro <nikolay.tyshchenko04@gmail.com>
2025-09-19 19:42:46 +0300

    feat: implement random quote functionality on home page & create search endpoint

--- src/components/layout/QuoteSection/QuoteSection.tsx
+++ src/components/layout/QuoteSection/QuoteSection.tsx
@@ -1,19 +1,25 @@
 import { TypographyP } from '../../ui/custom/typography';
 
-export function TwainQuote() {
+export async function TwainQuote() {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/quote/random`,
+    {
+      cache: 'no-store',
+    },
+  );
+
+  const quote = await res.json();
+
   return (
     <section className="flex w-full items-center justify-center mb-[70px]">
       <div className="w-full max-w-4xl px-4">
         <figure>
           <TypographyP className="border-l-2 border-stone-400 pl-6 font-serif text-xl not-italic italic text-stone-800">
-            He had discovered a{' '}
-            <em>great law of human action, without knowing it</em> --namely,
-            that in order to make <em>a man or a boy covet a thing</em>, it is
-            only necessary to make <em>the thing difficult to attain</em>.
+            {quote.quote}
           </TypographyP>
 
           <TypographyP className="mt-4 text-right !font-normal text-stone-600">
-            - The Adventures of Tom Sawyer by Mark Twain
+            - {quote.work} by {quote.author}
           </TypographyP>
         </figure>
       </div>
