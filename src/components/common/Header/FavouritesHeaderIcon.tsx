039a7143d6408c5bf4a1c95a8f10a3d060637e15
Aonarro <nikolay.tyshchenko04@gmail.com>
2025-09-25 23:19:31 +0300

    fix: minor changes

--- src/components/common/Header/FavouritesHeaderIcon.tsx
+++ src/components/common/Header/FavouritesHeaderIcon.tsx
@@ -66,7 +66,7 @@ export const FavouritesHeaderIcon = ({ userId }: Props) => {
       aria-label="Favourites"
       className="relative flex items-center justify-center text-custom-icons hover:text-custom-icons-hover transition-colors"
     >
-      <HeartIcon className="w-4 h-4 xl:w-6 xl:h-6" />
+      <HeartIcon className="w-6 h-6 xl:w-6 xl:h-6" />
       {displayCount > 0 && (
         <Badge
           className="absolute -top-1 -right-2  xl:-right-2 h-5 min-w-5 rounded-full px-0.5 font-mono tabular-nums flex items-center justify-center text-xs md:h-3 md:min-w-3 md:text-[8px] xl:h-5 xl:min-w-5 xl:text-xs md:-right-1"
